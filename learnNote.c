1f59762b3d3026d7903be2c991f5972d45ebea94
zqxl <416477054@qq.com>
2019-06-28 13:17:40 +0800

    分支操作“

--- learnNote.c
+++ learnNote.c
@@ -27,4 +27,9 @@ git push -u origin master									推送到远程，同时关联本地master和�
 /*
 分支管理
 */
-
+查看分支：				git branch
+创建分支：				git branch <name>
+切换分支：				git checkout <name>
+创建+切换分支：			git checkout -b <name>
+合并某分支到当前分支：	git merge <name>
+删除分支：				git branch -d <name>
